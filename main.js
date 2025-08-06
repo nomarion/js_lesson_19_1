7925d280ed41d1a059bcf97854e8ab4963717628
nomarion <novikovez1993@gmail.com>
2025-08-06 17:24:24 +0300

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const TOKEN = '2a06539b837057bb0d7fd0dd058b8e57';
+
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+    return getWeatherWidget()
+})
+
+void getWeatherWidget();
+
+async function getWeatherWidget() {
+    try {
+        const response =  await fetch('https://api.openweathermap.org/data/2.5/weather?q=Odessa&mode=html&appid=' + TOKEN);
+        const widget = await response.text()
+        document.getElementById('widget').innerHTML = widget;
+    } catch(error) {
+        console.error(error);
+    }
+}
+
